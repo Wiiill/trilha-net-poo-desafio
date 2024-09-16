46ac0e8f567f791cf812a9ffc892cfcc4e9410a8
William Rodrigues <william.rodrigues956@gmail.com>
2024-09-16 09:59:01 -0300

    Código atualizado, desafio completo

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+           Console.WriteLine($"Instalando {nomeApp} no Iphone");
+
+           for (int i = 0; i < 3; i++)
+        {
+            Thread.Sleep(1000);
+            Console.Write(".");
+        }
+                 
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
+    using System.Threading;
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+           Console.WriteLine($"Instalando {nomeApp} no Nokia");
+
+             for (int i = 0; i < 3; i++)
+        {
+            Thread.Sleep(1000);
+            Console.Write(".");
+        }
+            Console.WriteLine("");
+        }
     }
-}
+}   
